c45a5b35bd22b30ac61ffb4fe54f5fb516c7ebfc
Jun-ga <6dspoo@gmail.com>
2024-04-12 15:33:21 +0900

    Title: Carry 피하기, Time: 109ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240412/Carry 피하기/escaping-carry.py
+++ 240412/Carry 피하기/escaping-carry.py
@@ -1,24 +1,38 @@
 def recur(start):
     global cnt
-    if sum(res) > 10 and len(res) - 1 > cnt :
-        cnt = len(res)-1
+    #print(res)
+    if res and not cal(res) :
+        if len(res) - 1 > cnt:
+            cnt = len(res)-1
+        #print(cnt)
         return
 
     for i in range(start,N):
         res.append(num[i])
-        #print('app',res)
+        #print('app',i,' ',res)
         recur(i+1)
         res.pop()
-        #print('pop',res)
+        #print('pop',i,' ',res)
 
-
+def cal(res):
+    prev_num = str(sum(res[:-1]))
+    next_num = str(res[-1])
+    min_len = min(len(prev_num), len(next_num))
+    for i in range(min_len-1, -1 , -1):
+        if int(prev_num[i]) + int(next_num[i]) > 10:
+            #print('s')
+            return False
+    return True
+            
+        
+    
 
 N = int(input())
 num = []
 res = []
 cnt = 0
 for _ in range(N):
-    num.append(int(input()[-1]))
-
+    num.append(int(input()))
+#cal(res)
 recur(0)
 print(cnt)
